9b2d471c05925b56070b8e9dad81899d9f877c82
Chetan Dhotre <75079526+mrdc48@users.noreply.github.com>
2021-05-26 13:23:20 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,1 @@
-// on scroll animation
-AOS.init({
- duration: 650
-});
-
-const menuBtn = document.querySelector('.hamburger_menu')
-const navData = document.querySelector('.nav_links');
-
-menuBtn.addEventListener('click', ()=>{
- navData.classList.toggle('show');
-})
+AOS.init({duration:650});const menuBtn=document.querySelector(".hamburger_menu"),navData=document.querySelector(".nav_links");menuBtn.addEventListener("click",()=>{navData.classList.toggle("show")});const themeChanger=document.querySelector(".nav_logo"),fourth=document.querySelector(".footer_end"),third=document.querySelector(".footer"),second=document.querySelector(".nav_head"),first=document.querySelector(".content_div");themeChanger.addEventListener("click",()=>{first.classList.toggle("theme"),second.classList.toggle("new"),fourth.classList.toggle("new"),third.classList.toggle("theme")});
